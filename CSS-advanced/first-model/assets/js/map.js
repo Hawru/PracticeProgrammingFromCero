63105d7979f8cd620fec2565958e94ede8c24a29
Hawru <imharu01@gmail.com>
2023-11-01 11:18:43 -0500

    firts-model css advanced

---
+++ CSS-advanced/first-model/assets/js/map.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", (event) => {
+    setTimeout(() => {
+        document.querySelector("#load_map").innerHTML = `<iframe class="contact__iframe" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" allowfullscreen="" loading="lazy" 
+        referrerpolicy="no-referrer-when-downgrade"src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3959.371929502104!2d-73.07058402500186!3d7.082809192920137!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8e684066ccbb6a03%3A0xec6178a0ca30d571!2sParque%20Cerro%20del%20Sant%C3%ADsimo!5e0!3m2!1sen!2sco!4v1698175867824!5m2!1sen!2sco"></iframe>`;
+    }, 500)
+    
+})
